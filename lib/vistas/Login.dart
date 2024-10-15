5d9308d8c212a2f69897a047c6f8fd64e5b4a61b
JamesB15Code <jamesbrian1504@gmail.com>
2024-10-15 00:33:52 -0600

    Actualización de la pantalla de login: mejoras en la autenticación y validación de campos

--- lib/vistas/Login.dart
+++ lib/vistas/Login.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart'; // importar la dependencia 
-import 'tabs.dart'; 
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../componentes/CustomTextFormField.dart';
+import '../componentes/CustomElevatedButton.dart';
+import '../componentes/AlertMessage.dart'; // Importa el componente de alerta
+import '../servicios/UserService.dart'; // Importa el servicio de usuario
 
 class Login extends StatefulWidget {
   @override
@@ -9,102 +13,136 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
-  String _email = '';
-  String _password = '';
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool _isLoading = false;
 
-  void _login() {
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => TabsScreen()),
-    );
+  // Función para iniciar sesión
+  Future<void> _login() async {
+  if (_formKey.currentState!.validate()) {
+    setState(() {
+      _isLoading = true;
+    });
+
+    final url = Uri.parse('https://back-end-enfermera.vercel.app/api/auth/login');
+    final body = {
+      'correo': emailController.text,
+      'contraseña': passwordController.text,
+    };
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(body),
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        final token = data['token'];
+        await UserService().saveToken(token);
+
+        AlertMessage.show(
+          context: context,
+          message: 'Inicio de sesión exitoso',
+          type: MessageType.success,
+        );
+
+        // Navegar a la pantalla principal utilizando rutas nombradas
+        Navigator.pushReplacementNamed(context, '/tabs');
+      } else {
+        // Decodificar la respuesta de error de la API
+        final errorData = jsonDecode(response.body);
+        final errorMessage = errorData['message'] ?? 'Error al iniciar sesión';
+
+        // Mostrar el mensaje de error específico que devuelve la API
+        AlertMessage.show(
+          context: context,
+          message: errorMessage,
+          type: MessageType.error,
+        );
+      }
+    } catch (error) {
+      AlertMessage.show(
+        context: context,
+        message: 'Ocurrió un error. Intente de nuevo más tarde.',
+        type: MessageType.error,
+      );
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+}
+
+  // Función para navegar a la pantalla de registro
+  void _navigateToRegister() {
+    Navigator.pushNamed(context, '/register');
+  }
+
+  // Función para navegar a la pantalla de recuperación de contraseña
+  void _navigateToRecuperarContrasena() {
+    Navigator.pushNamed(context, '/recuperar');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // title: Text('Iniciar Sesión'),
+        centerTitle: true, 
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Form(
           key: _formKey,
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-
-              // Encabezado
-              Container(
+              Center(
                 child: Text(
-                  'Inicio de sesion',
+                  'Inicio de sesión',
                   style: TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-              SizedBox(height: 16.0), // Espacio entre el texto y el primer campo
-              // Agregamos un ícono de usuario circular
-              CircleAvatar(
-                radius: 40.0, // Tamaño del ícono circular
-                backgroundColor: Colors.grey[300], // Color de fondo
-                child: Icon(
-                  Icons.person, // Ícono de usuario
-                  size: 50.0, // Tamaño del ícono
-                  color: Colors.purple, // Color del ícono
-                ),
+              SizedBox(height: 16.0),
+              CustomTextFormField(
+                labelText: 'Correo electrónico',
+                validationType: 'gmail',
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
               ),
-              SizedBox(height: 16.0), // Espacio debajo del ícono
-
-              // Campo de correo electrónico
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Correo electrónico',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
+              SizedBox(height: 16.0),
+              PasswordField(
+                controller: passwordController,
+              ),
+              SizedBox(height: 16.0),
+              _isLoading
+                  ? Center(child: CircularProgressIndicator())
+                  : CustomElevatedButton(
+                      text: "Iniciar Sesión",
+                      onPressed: _login,
+                    ),
+              SizedBox(height: 16.0),
+              CustomElevatedButton(
+                text: "Registrarse",
+                onPressed: _navigateToRegister,
+              ),
+              SizedBox(height: 16.0),
+              Center(
+                child: TextButton(
+                  onPressed: _navigateToRecuperarContrasena,
+                  child: Text(
+                    '¿Olvidaste tu contraseña?',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.blue,
+                    ),
                   ),
                 ),
-                // ... (resto de las propiedades)
-              ),
-              SizedBox(height: 16.0), // Espacio entre los campos de texto
-
-              // Campo de contraseña
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Contraseña',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                obscureText: true, // Para ocultar el texto de la contraseña
-              ),
-              
-              SizedBox(height: 16.0), // Espacio entre los campos de texto y el botón
-
-              // Botón de inicio de sesión
-              ElevatedButton(
-                onPressed: _login,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.purple, // Color de fondo
-                  minimumSize: Size.fromHeight(50), // Tamaño mínimo del botón
-                ),
-                child: Text("Iniciar Sesión",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                ),
-                ),
-              ),
-              SizedBox(height: 16.0), // Espacio debajo del botón
-
-              // Enlace para recuperar la contraseña
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  '¿Olvidaste tu contraseña?',
-                  style: TextStyle(
-                    fontSize: 16,
-                ),
-                  ),                
               ),
             ],
           ),
