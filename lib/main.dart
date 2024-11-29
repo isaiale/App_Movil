d3b648c1a9ec4581e33ea15b617ab133efc3ed79
isaiale <isaialef20@gmail.com>
2024-11-28 20:48:15 -0600

    actuaLIzacion de app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'vistas/inicio.dart'; // Pantalla de inicio (Splash)
 import 'vistas/login.dart'; // Pantalla de login
 // import 'vistas/tabs.dart'; // Pantalla principal (con pestañas)
@@ -10,8 +11,20 @@ import 'vistas/Productos.dart';
 import 'vistas/compras.dart';
 import 'vistas/Home.dart';
 
-void main() {
-  runApp(MyApp());
+
+Future<void> main() async {
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = 'https://3c00a4d9f334ee73c9d350fcd1dc90bd@o4508372929609728.ingest.us.sentry.io/4508373476507648';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+      // The sampling rate for profiling is relative to tracesSampleRate
+      // Setting to 1.0 will profile 100% of sampled transactions:
+      options.profilesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
